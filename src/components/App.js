b7b9d9c780b1afd5475f9a511acde8e5a4f115d4
thaliadettenborn <thalia.born@gmail.com>
2020-10-15 17:33:17 -0300

    adding react-router structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+import Header from './Header';
+import Main from './Main';
+import Pokemon from './Pokemon';
+
 
 export default function App(){
   return (
-    
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path='/' exact>
+          <Main />
+        </Route>
+        <Route path='/pokemon/:id' exact>
+          <Pokemon />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   )
 }
